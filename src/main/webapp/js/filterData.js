1c07291cf249a188adcc936836bb8bb908be5113
lauv ari <198719993@qq.com>
2025-01-02 09:29:55 +0800

    优化filter-data展示效果

--- src/main/webapp/js/filterData.js
+++ src/main/webapp/js/filterData.js
@@ -3,6 +3,7 @@ const $filteredDataContainer= $('#filtered-data-container')
 const categoryFilter = document.getElementById('category-filter')
 const productFilter = document.getElementById('product-filter')
 categoryFilter.addEventListener('change', function() {
+    $filteredDataContainer.html('')
     $.ajax({
         type: "get",
         url:'http://localhost:8080/filterDataByCategory?category=' + categoryFilter.value,
@@ -20,6 +21,17 @@ categoryFilter.addEventListener('change', function() {
                 option.value = d.productId
                 option.text = `${d.name}: ${d.productId}`
                 productFilter.appendChild(option)
+                $.ajax({
+                    type:'get',
+                    url:'http://localhost:8080/filterDataByProduct?product=' + d.productId,
+                    success(data) {
+                        showData(data)
+                        console.log(data)
+                    },
+                    error(errorMsg) {
+                        console.log(errorMsg)
+                    }
+                })
             }
         },
         error(errorMsg) {
@@ -27,26 +39,32 @@ categoryFilter.addEventListener('change', function() {
         }
     })
 })
+
+function showData(data, level) {
+    //更新页面
+    main.innerHTML = ''
+    main.style.display = 'none'
+    if (level === 2) {
+        $filteredDataContainer.html('')
+    }
+    for (let d of data) {
+        let html = `
+                            <div id="${d.itemId}" class="filtered-data-entry" 
+                                    title=" ${d.attribute1}\n name: ${d.product.name}\n list price: ${d.listPrice}\n item ID: ${d.itemId}">
+                                ${d.product.description}
+                                <div>${d.product.name} $ ${d.listPrice}</div>
+                                <button class="add-to-item-btn" data-item-id="${d.itemId}">Add to cart</button>
+                            </div>
+                            `
+        $filteredDataContainer.append(html)
+    }
+}
 productFilter.addEventListener('change', function() {
     $.ajax({
         type:'get',
         url:'http://localhost:8080/filterDataByProduct?product=' + productFilter.value,
         success(data) {
-            //更新页面
-            main.innerHTML = ''
-            main.style.display = 'none'
-            $filteredDataContainer.html('')
-            for (let d of data) {
-                let html = `
-                                <div id="${d.itemId}" class="filtered-data-entry" 
-                                        title=" ${d.attribute1}\n name: ${d.product.name}\n list price: ${d.listPrice}\n item ID: ${d.itemId}">
-                                    ${d.product.description}
-                                    <div>${d.product.name} $ ${d.listPrice}</div>
-                                    <button class="add-to-item-btn" data-item-id="${d.itemId}">Add to cart</button>
-                                </div>
-                                `
-                $filteredDataContainer.append(html)
-            }
+            showData(data, 2)
             console.log(data)
         },
         error(errorMsg) {
